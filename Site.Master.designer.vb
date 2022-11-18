9b9ee1e64fda95b681ded9509fc455b40c768377
cordonezjolon <cordonezjolon@gmail.com>
2022-11-18 00:06:23 -0600

    [FormularioDefault] Se crea formulario default para manejo de solicitudes notificaicones y previos.

--- Site.Master.designer.vb
+++ Site.Master.designer.vb
@@ -14,6 +14,15 @@ Option Explicit On
 Partial Public Class SiteMaster
 
     '''<summary>
+    '''Control repMenu.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents repMenu As Global.System.Web.UI.WebControls.Repeater
+
+    '''<summary>
     '''Control MainContent.
     '''</summary>
     '''<remarks>
--- Site.Master.vb
+++ Site.Master.vb
@@ -1,6 +1,33 @@
 ﻿Public Class SiteMaster
     Inherits MasterPage
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
-
+        repMenu.DataSource = creaMenu()
+        repMenu.DataBind()
     End Sub
+
+
+    Private Function creaMenu() As DataTable
+        Dim menu As DataTable = New DataTable()
+        menu.Columns.Add("PageFile", GetType(String))
+        menu.Columns.Add("DisplayName", GetType(String))
+        menu.Columns.Add("Icon", GetType(String))
+        Dim row As DataRow = menu.NewRow()
+        row("PageFile") = "home.aspx"
+        row("DisplayName") = "Nueva solicitud"
+        row("Icon") = "fa-plus"
+        menu.Rows.Add(row)
+        row = menu.NewRow()
+        row("PageFile") = "Accounts.aspx"
+        row("DisplayName") = "Mis Solicitudes"
+        row("Icon") = "fa-list"
+
+        menu.Rows.Add(row)
+        row = menu.NewRow()
+        row("PageFile") = "Logout.aspx"
+        row("DisplayName") = "Perfil"
+        row("Icon") = "fa-user"
+
+        menu.Rows.Add(row)
+        Return menu
+    End Function
 End Class
