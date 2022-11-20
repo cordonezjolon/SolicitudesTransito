6a40ade7630af6b5dee88a32de784bef29e6838e
cordonezjolon <cordonezjolon@gmail.com>
2022-11-19 19:23:39 -0600

    [FormularioInicial] Cambios para integrar dinamismo a pagina inicial y tablas.

---
+++ Clases/Tramites/Tramite.vb
@@ -0,0 +1,14 @@
+﻿Public Class Tramite
+
+    Inherits SerializedClass
+    Public Property id_tramite As String
+    Public Property id_tipo_tramite As String
+    Public Property identificador As String
+    Public Property id_flujo As String
+    Public Property id_usuario_creacion As String
+    Public Property id_primer_usuario_sorteado As String
+    Public Property estado As String
+    Public Property descripcion As String
+    Public Property documentos As List(Of Documento)
+
+End Class
